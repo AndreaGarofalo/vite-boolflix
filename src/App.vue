435844c3ab4d9d463a4fdac3cc25013ee9d9558a
AndreaGarofalo <andreasjobs94@outlook.it>
2023-01-29 14:08:40 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+export default {
+  name: "Boolflix",
+  components: { AppHeader },
+};
+</script>
+
+<template>
+  <AppHeader />
+</template>
+
+<style></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+  name: "Boolflix Header",
+};
+</script>
+
+<template>
+  <header>
+    <div class="container">
+      <div class="header-content">
+        <h2>Boolflix</h2>
+        <div class="searchbar">
+          <input type="text" />
+          <button class="text-light">Cerca</button>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+header {
+  background-color: black;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 60px;
+  .container {
+    height: 100%;
+    .header-content {
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      h2 {
+        color: red;
+      }
+      button {
+        background-color: red;
+      }
+    }
+  }
+}
+</style>
